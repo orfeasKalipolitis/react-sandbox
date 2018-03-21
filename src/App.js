5061f692dd836f79e450f1e929806700ec33b7b6
Orfeas Kalipolitis <csd3285@csd.uoc.gr>
2018-03-21 11:59:25 +0200

    Implemented history functionality on TTT, finished tutorial

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+/**
+ * Returns a button with class "square" and
+ * value, onClick based on props
+ */
 function Square(props) {
   return (
     <button className="square" onClick={props.onClick}>
@@ -14,65 +18,16 @@ function Square(props) {
  * Renders a tick-tack-toe board
  */
 class Board extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      squares: Array(9).fill(null),
-      xNext: true,
-    };
-  }
-
-  calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
-}
-
-  handleClick(i) {
-    const squares = this.state.squares.slice();
-    if (this.calculateWinner(squares) || squares[i]) {
-      return;
-    }
-    squares[i] = this.state.xNext ? 'X' : 'O';
-    this.setState({
-      squares: squares,
-      xNext: !this.state.xNext,
-    });
-  }
-
   renderSquare(i) {
     return <Square
-              value={this.state.squares[i]}
-              onClick={() => this.handleClick(i)} 
+              value={this.props.squares[i]}
+              onClick={() => this.props.onClick(i)} 
             />;
   }
 
   render() {
-    const winner = this.calculateWinner(this.state.squares);
-    let status;
-    if (winner) {
-      status = 'Winner : ' + winner;
-    } else {
-      status = 'Next player: ' + (this.state.xNext ? 'X' : 'O');
-    }
-
     return (
       <div>
-        <div className="status">{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -93,22 +48,129 @@ class Board extends React.Component {
   }
 }
 
+/**
+ * Creates a Tick-Tack-Toe game instance
+ * where two players can play (X and O).
+ * There is a history implementation, which
+ * means you can back-track to whichever moment
+ * in the current game (not after a change has been 
+ * made in the timeline)
+ */
 class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      history: [{
+        squares: Array(9).fill(null),
+      }],
+      xNext: true,
+      stepNumber: 0,
+    };
+  }
+
+  /**
+   * Checks a 0-indexed array of TTT square to see whether or not
+   * there is a winner.
+   * Returns the winner if there is one.
+   * Returns null otherwise.
+   * @param {Array[9]} squares 
+   */
+  calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
+  /**
+   * Handles the onClick functionality of each square
+   * @param {int} i index of square clicked
+   */
+  handleClick(i) {
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+    if (this.calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xNext ? 'X' : 'O';
+    this.setState({
+      history: history.concat([{
+        squares: squares,
+      }]),
+      xNext: !this.state.xNext,
+      stepNumber: history.length,
+    }); 
+  }
+
+  /**
+   * Time travels the game to "step"
+   * @param {int} step when
+   */
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xNext: (step % 2) === 0,
+    });
+  }
+
   render() {
+    const history = this.state.history;
+    const current = history[this.state.stepNumber];
+    const winner = this.calculateWinner(current.squares);
+
+    const moves = history.map((step, move) => {
+      const desc = move ?
+        'Go to move #' + move :
+        'Go to game start';
+
+      return (
+        <li key={move}>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      );
+    });
+
+    let status;
+    if (winner) {
+      status = "Winner: " + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xNext ? 'X' : 'O');
+    }
+
     return (
       <div className="game">
         <div className="game-board">
-          <Board />
+          <Board 
+            squares={current.squares}
+            onClick={(i) => this.handleClick(i)}
+          />
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
+          <div>{status}</div>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
   }
 }
 
+/**
+ * Main App class
+ */
 class App extends Component {
   render() {
     return (
